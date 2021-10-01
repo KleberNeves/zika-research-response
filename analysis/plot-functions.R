1ff575af9d996ed7874c568cce395e9aaba6e9a3
KleberNeves <kleber.na@gmail.com>
2021-10-01 15:14:00 -0300

    Add triangle and cognitive career plots

--- analysis/plot-functions.R
+++ analysis/plot-functions.R
@@ -513,3 +513,153 @@ plot_cite_rate_by_mesh_cat_FAPERJ = function (AUTHOR_DATA, FAPERJ_NETWORK_INFO) 
       ) + labs(title = paste("FAPERJ Network", .y$FAPERJ_Net))
     })
 }
+
+
+cognitiveCareerPlot = function(author_filename, base.size = 10, n = 30, periods = periods = c(2015), period.names = c("Before Zika Outbreak", "After Zika Outbreak")) {
+  M = bibliometrix::convert2df(author_filename, dbsource = "isi", format = "plaintext")
+    
+    # Filter the papers that will appear, if not showing all of them.
+    if (nrow(M) > n) {
+      # Possibilidade: incluir todas as revisões, porque elas são marcos
+      
+      # Picks the most cited for each year.
+      topyear = (M %>% group_by(PY) %>% top_n(1,TC))$DI
+      k = min(n, nrow(M)) - length(topyear)
+      
+      # Picks the most cited overall
+      topall = (M %>% filter(!(DI %in% topyear)) %>% top_n(k,TC))$DI
+      
+      # Filters
+      toinclude = unique(c(topyear, topall))
+      M = M[M$DI %in% toinclude, ]
+    }
+  
+  # Makes the adjancency matrix and graph for the bibliographic coupling network
+  ADJ = as.matrix(biblioNetwork(M, analysis = "coupling", network = "references", sep = ";", shortlabel = F))
+  NET = graph_from_adjacency_matrix(ADJ)
+  
+  # Creates the layout from graphs, to be able to specify positioning of nodes
+  NET = simplify(NET, remove.multiple = T, remove.loops = T)
+  layout_m = create_layout(NET, layout = "auto")
+  
+  # Extract numeric years
+  layout_m$Year = as.numeric(str_extract(V(NET)$name, "[0-9]{4,}"))
+  Years = layout_m$Year
+  
+  # Defines size of nodes
+  # layout_m$node.size = 10 * (base.size + 2 * base.size * M$TC / max(M$TC))
+  # TODO for some reason, setting node.size via aes(size = node.size) from layout_m does not work
+  V(NET)$node.size = (base.size / 2 + 2 * base.size * M$TC / max(M$TC))
+  
+  # Name of nodes is the paper title, truncated at 50 characters
+  layout_m$id = toupper(M$TI)
+  layout_m$name.truncated = str_sub(M$TI, 1, 60)
+  
+  # Breaks the label in three lines for longer titles and sets node properties
+  layout_m$name.label = ifelse(V(NET)$node.size < 0.8 * base.size,
+                               "", paste0(
+                                 str_sub(M$TI, 1, 20),"\n",
+                                 str_sub(M$TI, 21, 40),"\n",
+                                 str_sub(M$TI, 41, 60),"\n"
+                               )
+  )
+  
+  # Separates papers in clusters and add cluster data to layout, to use for y pos
+  V(NET)$id = layout_m$id # will use to check for cluster belonging in the loop below
+  dg = decompose.graph(NET, mode = "weak")
+  layout_m$cluster = 0
+  for (k in 1:length(dg)) {
+    dec = dg[[k]]
+    layout_m$cluster = ifelse(layout_m$id %in% V(dec)$id, k, layout_m$cluster)
+  }
+  
+  # Join isolated nodes in a single cluster
+  d = layout_m %>% group_by(cluster) %>% summarise(n = n())
+  isolated = (d %>% filter(n == 1))$cluster
+  layout_m$cluster = ifelse(layout_m$cluster %in% isolated, 0, layout_m$cluster)
+  # browser()
+  
+  # Sets position of nodes based on cluster and year
+  layout_m$x = layout_m$Year
+  posy = layout_m %>% group_by(Year) %>% mutate(py = 1:n()) %>% mutate(maxpy = max(py))
+  layout_m$py = (posy$py) / (posy$maxpy + 1)
+  layout_m$py = layout_m$py + ifelse(layout_m$Year %% 2 == 0, 0.5, 0)
+  pheight = max(posy$maxpy) * base.size / 2
+  layout_m$y = layout_m$py * pheight + rnorm(nrow(layout_m), 0, layout_m$py * 1.5)
+  layout_m$y = layout_m$y - min(layout_m$y)
+  
+  # Builds ggplot
+  g = ggraph(layout_m)
+  
+  if (!is.null(periods)) {
+    g = g + geom_vline(xintercept = periods, linetype = "dashed", color = "black")
+  }
+  # browser()
+  g = g +
+    geom_edge_link(width = 1, check_overlap = T, edge_alpha = 0.8, color = "grey") +
+    geom_node_point(aes(color = as.factor(cluster)), size = V(NET)$node.size, alpha = 0.8) +
+    geom_node_text(aes(label = name.label), size = 2.2, 
+                   repel = F, color = "black", alpha = 1) + 
+    scale_x_continuous(labels = as.character(seq(min(Years), max(Years))), breaks = seq(min(Years), max(Years)), expand = c(.1, .1)) +
+    labs(title = "Cognitive Career Plot (Reference Coupling)", x = "", y = "")
+  
+  # Adicionar retângulos e nomes de períodos
+  if (!is.null(periods)) {
+    periods = c(min(Years), periods, c(max(Years)))
+    tly = (max(layout_m$y) * 1.15)
+    for (i in 1:(length(periods)-1)) {
+      g = g +
+        annotate("rect", xmin = periods[i], ymin = (tly),
+                 xmax = periods[i+1], ymax = (tly*0.98), fill = "grey80", color = "black") +
+        annotate("text", label = period.names[i], x = (periods[i] + periods[i+1]) / 2,
+                 y = (tly * 0.95), size = 3)
+    }
+  }
+  # browser()
+  g + theme_minimal() + theme(
+    legend.position = "none",
+    
+    panel.background = element_rect(fill = "grey97", color = "grey97"),
+    panel.grid.minor.y = element_blank(), panel.grid.major.y = element_blank(),
+    panel.grid.minor.x = element_blank(),
+    
+    axis.line.y = element_blank(), axis.text.y = element_blank(),
+    axis.ticks.y = element_blank(), axis.title.y = element_blank(),
+    
+    axis.title.x = element_blank(), axis.line.x = element_blank(),
+    axis.text.x = element_text(face = "bold")
+  )
+}
+
+triangle_plot = function (tri_data, tri_cols, tri_labs = NULL, plot_translation_axis = F, add_density = T) {
+  if (is.null(tri_labs)) { tri_labs =  tri_cols }
+  p = ggtern::ggtern(data = tri_data) +
+    ggplot2::aes_string(x = tri_cols[2], y = tri_cols[1], z = tri_cols[3]) +
+    ggplot2::geom_point(size = 2) +
+    ggplot2::labs(x = tri_labs[2], y = tri_labs[1], z = tri_labs[3]) +
+    ggtern::theme_nogrid_minor()
+  
+  if (add_density) {
+    p = p + ggtern::geom_density_tern()
+  }
+  
+  if (plot_translation_axis) {
+    p = p + ggtern::geom_Risoprop(value = 0.5, linetype = "dashed", color = "blue")
+  }
+  
+  p
+}
+
+biomed_triangle_plot = function (tri_data) {
+  triangle_plot(tri_data, c("A","C","H"), c("Animal","Mol./Cell.","Human"), T, F)
+}
+
+count_data_for_triangle = function (mesh_categories, ref_categories) {
+  DF = data.frame(Category = mesh_categories)
+  cat_cols = purrr::map_dfc(ref_categories, function (tri_cat) {
+    df = data.frame(x = sum(stringr::str_detect(DF$Category, tri_cat)))
+    colnames(df) = tri_cat
+    df
+  })
+  cat_cols
+}
